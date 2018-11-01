cb78e18e8cf7d0cf4877538907dd532872aaec34
Danijel Sugar <danijel.sugar@outlook.com>
2018-11-01 17:13:21 +0100

    Authorization checks if entered usrename exist in database and checks whether the entered password corresponds to the password in database. If everything is correct user is redirected to chat home page. For incorrect entries usire is provided with the appropriate message.

---
+++ auth.php
@@ -0,0 +1,27 @@
+<?php 
+	print_r($_POST);
+	if(!isset($_POST["username"])){
+		exit;
+	}
+
+	include_once "config.php";
+
+	if($_POST["username"]===""){
+		header("location: index.php?msg=2");
+		exit;
+	}
+
+	$query = $connect->prepare("select * from signup where username=:username");
+	$query->execute(array("username"=>$_POST["username"]));
+
+	$user = $query->fetch(PDO::FETCH_OBJ);
+	print_r($user);
+
+	if($user !=null && $user->pass==$_POST["password"]){
+		//pusti dalje
+		$user->pass="";
+		$_SESSION["user"]=$user;
+		header("location: private/home.php");
+	}else {
+		header("location: index.php?msg=1");
+	}
