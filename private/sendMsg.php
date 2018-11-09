93ad655b82b9cb0ed15646fe7e5630f9b1b240d7
Danijel Sugar <danijel.sugar@outlook.com>
2018-11-09 22:51:18 +0100

    Added line of code to convert special characters to HTML entities

--- private/sendMsg.php
+++ private/sendMsg.php
@@ -2,12 +2,14 @@
 
 	include_once "../config.php";
 
-	
+
+	$msg = $_POST["msg"];
+	$msg = htmlspecialchars($msg);
 
 	$query = $connect->prepare("insert into posts(msg,username) values 
 								(:msg,:username)");
 	$query->execute(array(
-		"msg"=>$_POST["msg"],
+		"msg"=>$msg,
 		"username"=>$_POST["id"]
 	));
 
