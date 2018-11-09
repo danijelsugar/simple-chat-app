9d0c35587c77b30eb59a40428956e7343a19ad38
Danijel Sugar <danijel.sugar@outlook.com>
2018-11-09 22:43:52 +0100

    Dont send json object as response anymore.

--- private/allPosts.php
+++ private/allPosts.php
@@ -4,7 +4,13 @@
 
 	$query = $connect->prepare("select a.id, a.msg, b.username, a.published from posts a 
 								inner join signup b 
-								on a.username=b.uid");
+								on a.username=b.uid 
+								order by a.published");
 	$query->execute();
 	$result = $query->fetchAll(PDO::FETCH_OBJ);
-	echo json_encode($result);
+	foreach($result as $row){
+		echo "<div>";
+		echo "<p class='username'>" . $row->username . "</p>" . "<p class='msg'>" . $row->msg . "</p>" . "<p class='published'>" . $row->published . "</p>";
+		echo "</div>";
+	}
+	//echo json_encode($result);
